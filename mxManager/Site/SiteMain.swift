c9fbe3fa42ea6fbfb14b515016447597bebab96b
bezumkin <bezumkin@yandex.ru>
2015-02-05 18:06:59 +0700

    Removed buttons from SiteMain Added row menu in SitesList

--- mxManager/Site/SiteMain.swift
+++ mxManager/Site/SiteMain.swift
@@ -12,33 +12,16 @@ class SiteMain: DefaultTable {
 
 	var popup: UIViewController?
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
-		self.updateTitle()
-		self.loadRows()
-
-		NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateSettings:", name:"SiteUpdated", object: nil)
-	}
-
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-		if segue.identifier == "ShowSettings" {
-			var controller = segue.destinationViewController as SiteSettings
-			controller.data = self.data
-		}
-		else if segue.identifier == "ExitView" {
-			// Nothing
-		}
-		else {
-			let cell = sender as DefaultCell
-			var controller = segue.destinationViewController as DefaultView
-			controller.data = self.data
+		self.navigationItem.backBarButtonItem = UIBarButtonItem.init(title:"", style:UIBarButtonItemStyle.Plain, target:nil, action:nil)
 
-			controller.navigationItem.title = cell.textLabel?.text
-			self.navigationItem.backBarButtonItem = UIBarButtonItem.init(title:"", style:UIBarButtonItemStyle.Plain, target:nil, action:nil)
-		}
+		let cell = sender as DefaultCell
+		var controller = segue.destinationViewController as DefaultView
+		controller.data = self.data
+		controller.navigationItem.title = cell.textLabel?.text
 	}
 
+	// For clear cache log
 	override func willRotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
 		if self.popup != nil {
 			let original_width = self.view.frame.size.width
@@ -64,7 +47,6 @@ class SiteMain: DefaultTable {
 			Utils().showSpinner(self.view)
 		}
 		let site = Site.init(params: self.data) as Site
-
 		self.request = [
 				"mx_action": "auth",
 				"username": self.data["user"] as String,
@@ -133,42 +115,6 @@ class SiteMain: DefaultTable {
 				Utils().hideSpinner(self.view)
 				Utils().alert("", message:data["message"] as String, view:self)
 			})
-		}
-	}
-
-	func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-		return self.tableFooterView?
-	}
-
-	func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-		return 50
-	}
-
-	@IBAction func editSite() {
-		self.performSegueWithIdentifier("ShowSettings", sender: nil)
-	}
-
-	@IBAction func deleteSite() {
-		Utils().confirm("warning", message:"site_delete_confirm", view: self, { _ in
-			if Utils().removeSite(self.data["key"] as NSString) {
-				self.performSegueWithIdentifier("ExitView", sender: self)
-			}
-		})
-	}
-
-	func updateSettings(event: AnyObject) {
-		if event.object != nil {
-			if let object = event.object as? NSDictionary {
-				self.data = object
-				self.updateTitle()
-				self.loadRows()
-			}
-		}
-	}
-
-	func updateTitle() {
-		if self.data["site"] != nil {
-			self.title = self.data["site"] as NSString
 		}
 	}
 
--- mxManager/Site/SitesList.swift
+++ mxManager/Site/SitesList.swift
@@ -16,21 +16,23 @@ class SitesList: DefaultTable {
 		super.viewDidLoad()
 
 		NSNotificationCenter.defaultCenter().addObserver(self, selector: "loadRowsFromEvent", name:"SiteAdded", object: nil)
-		NSNotificationCenter.defaultCenter().addObserver(self, selector: "loadRowsFromEvent", name:"SiteUpdated", object: nil)
 		NSNotificationCenter.defaultCenter().addObserver(self, selector: "loadRowsFromEvent", name:"SiteDeleted", object: nil)
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "loadRowsFromEvent", name:"SiteUpdated", object: nil)
 	}
 
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		self.navigationItem.backBarButtonItem = UIBarButtonItem.init(title:"", style:UIBarButtonItemStyle.Plain, target:nil, action:nil)
 
-		if segue.identifier? == "ShowSite" {
+		if segue.identifier? == "ShowSettings" {
+			var controller = segue.destinationViewController as SiteSettings
+			controller.data = sender as NSDictionary
+		}
+		else if segue.identifier? == "ShowSite" {
 			let cell = sender as DefaultCell
 			var controller = segue.destinationViewController as SiteMain
 			controller.data = cell.data as NSDictionary
+			controller.title = cell.data["site"] as? String
 		}
-	}
-
-	@IBAction func unwindFromViewController(sender: UIStoryboardSegue) {
 	}
 
 	override func loadRows(spinner: Bool = false) {
@@ -53,6 +55,38 @@ class SitesList: DefaultTable {
 		cell.template(idx: indexPath.row)
 
 		return cell
+	}
+
+	func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
+		let data = self.rows[indexPath.row] as NSDictionary
+		if data["key"] == nil {
+			println("No key in \(data)")
+			return []
+		}
+		let key = data["key"] as String
+
+		let edit: UITableViewRowAction = UITableViewRowAction.init(style: UITableViewRowActionStyle.Default, title: "      ") {
+			(action, indexPath) -> Void in
+			tableView.editing = false
+
+			self.performSegueWithIdentifier("ShowSettings", sender: data)
+		}
+		edit.backgroundColor = UIColor(patternImage: UIImage(named:"btn-edit")!)
+
+		let delete: UITableViewRowAction = UITableViewRowAction.init(style: UITableViewRowActionStyle.Normal, title: "      ") {
+			(action, indexPath) -> Void in
+			tableView.editing = false
+
+			Utils().confirm("warning", message:"site_delete_confirm", view: self, { _ in
+				if Utils().removeSite(key) {}
+			})
+		}
+		delete.backgroundColor = UIColor(patternImage: UIImage(named:"btn-delete")!)
+
+		return [delete, edit]
+	}
+
+	func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
 	}
 
 }
