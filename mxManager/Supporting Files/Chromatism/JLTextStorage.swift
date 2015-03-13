b34e6a0c3cd1fa4000dfb7496bbc1136097d8bb3
bezumkin <bezumkin@yandex.ru>
2015-03-13 20:07:52 +0700

    Added Chromantism

---
+++ mxManager/Supporting Files/Chromatism/JLTextStorage.swift
@@ -0,0 +1,63 @@
+//
+//  JLTextStorage.swift
+//  Chromatism
+//
+//  Created by Johannes Lund on 2014-07-26.
+//  Copyright (c) 2014 anviking. All rights reserved.
+//
+
+import UIKit
+
+public class JLTextStorage: NSTextStorage {
+    
+    public var documentScope: JLDocumentScope
+    
+    init(documentScope: JLDocumentScope) {
+        self.documentScope = documentScope
+        super.init()
+        self.documentScope.cascadeAttributedString(self)
+    }
+
+    required public init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Syntax Highlighting
+    
+    public override func processEditing() {
+        if let range = editedLineRange {
+            let layoutManager = layoutManagers[0] as NSLayoutManager
+            //println("Non Contigigous Layout: \(layoutManager.hasNonContiguousLayout)")
+            let editedLineIndexSet = NSIndexSet(indexesInRange: range)
+            documentScope.perform(editedLineIndexSet)
+            editedLineRange = nil
+        }
+        super.processEditing()
+    }
+    
+    // MARK: Text Storage Backing
+    
+    private var backingStore = NSMutableAttributedString()
+    private var editedLineRange: NSRange?
+    
+    public override var string: String { return backingStore.string }
+    
+    public override func attributesAtIndex(location: Int, effectiveRange range: NSRangePointer) -> [NSObject : AnyObject] {
+        return backingStore.attributesAtIndex(location, effectiveRange: range)
+    }
+    
+    public override func replaceCharactersInRange(range: NSRange, withString str: String) {
+        let actions = NSTextStorageEditActions.EditedCharacters | NSTextStorageEditActions.EditedAttributes
+        let delta = str.utf16Count - range.length
+        edited(actions, range: range, changeInLength: delta)
+        backingStore.replaceCharactersInRange(range, withString: str)
+        editedLineRange = (string as NSString).lineRangeForRange(editedRange)
+        documentScope.invalidateAttributesInIndexes(NSIndexSet(indexesInRange: range))
+        documentScope.shiftIndexesAtLoaction(range.end, by: delta)
+    }
+    
+    public override func setAttributes(attrs: [NSObject : AnyObject]!, range: NSRange) {
+        backingStore.setAttributes(attrs, range: range)
+        edited(.EditedAttributes, range: range, changeInLength: 0)
+    }
+}
---
+++ mxManager/Supporting Files/Chromatism/JLTextViewController.swift
@@ -0,0 +1,82 @@
+//
+//  JLTextViewController.swift
+//  Chromatism
+//
+//  Created by Johannes Lund on 2014-07-18.
+//  Copyright (c) 2014 anviking. All rights reserved.
+//
+
+import UIKit
+
+public class JLTextViewController: UIViewController {
+    
+    public var textView: JLTextView
+    
+    public required init(text: String, language: JLLanguageType, theme: JLColorTheme) {
+        textView = JLTextView(language: language, theme: theme)
+        textView.text = text
+        super.init(nibName: nil, bundle: nil)
+        registerForKeyboardNotifications()
+    }
+
+    required public init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        unregisterForKeyboardNotifications()
+    }
+    
+    override public func loadView()  {
+        view = textView
+    }
+
+    // MARK: Content Insets and Keyboard
+    
+    func registerForKeyboardNotifications()
+    {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterForKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    // Called when the UIKeyboardDidShowNotification is sent.
+    func keyboardWasShown(notification: NSNotification) {
+        // FIXME: ! could be wrong
+        let info = notification.userInfo!
+        let scrollView = self.textView
+        let kbSize = (info[UIKeyboardFrameBeginUserInfoKey] as NSValue).CGRectValue().size;
+        
+        var contentInsets = scrollView.contentInset;
+        contentInsets.bottom = kbSize.height;
+        scrollView.contentInset = contentInsets;
+        scrollView.scrollIndicatorInsets = contentInsets;
+        
+        // FIXME: ! could be wrong
+        var point = textView.caretRectForPosition(textView.selectedTextRange!.start).origin;
+        point.y = min(point.y, self.textView.frame.size.height - kbSize.height);
+        
+        var aRect = self.view.frame;
+        aRect.size.height -= kbSize.height;
+        if (!CGRectContainsPoint(aRect, point) ) {
+            
+            var rect = CGRectMake(point.x, point.y, 1, 1)
+            rect.size.height = kbSize.height
+            rect.origin.y += kbSize.height
+            textView.scrollRectToVisible(rect, animated: true)
+        }
+    }
+    
+    // Called when the UIKeyboardWillHideNotification is sent
+    func keyboardWillBeHidden(notification: NSNotification) {
+        var contentInsets = textView.contentInset;
+        contentInsets.bottom = 0;
+        textView.contentInset = contentInsets;
+        textView.scrollIndicatorInsets = contentInsets;
+        textView.contentInset = contentInsets;
+        textView.scrollIndicatorInsets = contentInsets;
+    }
+}
