6e7f849129e0546bd4fac045dd784bd5e68bc314
bezumkin <bezumkin@yandex.ru>
2015-03-25 14:53:46 +0700

    Improved error log

--- mxManager/Site/ErrorLog.swift
+++ mxManager/Site/ErrorLog.swift
@@ -10,63 +10,81 @@ import UIKit
 
 class ErrorLog: DefaultView {
 
-	@IBOutlet var textLabel: UILabel?
+	@IBOutlet var textView: UITextView!
+	var refreshBtn: UIBarButtonItem?
+	var clearBtn: UIBarButtonItem?
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		self.addRightButtons()
 
 		self.refreshLog()
 	}
 
-	@IBAction func refreshLog() {
+	func addRightButtons() {
+		self.refreshBtn = UIBarButtonItem.init(image: UIImage.init(named: "icon-refresh")?, style: UIBarButtonItemStyle.Plain, target: self, action: "refreshLog")
+		self.clearBtn = UIBarButtonItem.init(image: UIImage.init(named: "icon-trash")?, style: UIBarButtonItemStyle.Plain, target: self, action: "clearLog")
+
+		self.navigationItem.setRightBarButtonItems([self.refreshBtn!, self.clearBtn!], animated: false)
+	}
+
+	func refreshLog() {
 		let parameters = [
-			"mx_action": "main/errorlog/get"
+				"mx_action": "main/errorlog/get"
 		]
 
 		Utils().showSpinner(self.view)
 		self.Request(parameters, {
-			data in
+			(data: NSDictionary!) in
 			Utils().hideSpinner(self.view)
 			self.setLog(data["data"] as NSDictionary)
 		}, {
-			data in
+			(data: NSDictionary!) in
 			Utils().hideSpinner(self.view)
-			Utils().alert("", message:data["message"] as String, view:self)
+			Utils().alert("", message: data["message"] as String, view: self)
 		})
 	}
 
-	@IBAction func clearLog() {
+	func clearLog() {
 		let parameters = [
 				"mx_action": "main/errorlog/clear"
 		]
 
-		Utils().showSpinner(self.view)
-		self.Request(parameters, {
-			data in
-			Utils().hideSpinner(self.view)
-			self.setLog(data["data"] as NSDictionary)
-		}, {
-			data in
-			Utils().hideSpinner(self.view)
-			Utils().alert("", message:data["message"] as String, view:self)
+		Utils().confirm("", message: "error_log_clear_confirm" as String, view: self, closure: {
+			_ in
+			Utils().showSpinner(self.view)
+			self.Request(parameters, {
+				(data: NSDictionary!) in
+				Utils().hideSpinner(self.view)
+				self.setLog(data["data"] as NSDictionary)
+			}, {
+				(data: NSDictionary!) in
+				Utils().hideSpinner(self.view)
+				Utils().alert("", message: data["message"] as String, view: self)
+			})
 		})
 	}
 
-	func setLog(data:NSDictionary) {
+	func setLog(data: NSDictionary) {
 		if data["log"] != nil {
-			var log:NSString
+			var log = ""
+
 			if data["tooLarge"] as Bool {
 				log = Utils().lexicon("error_log_too_large")
 			}
 			else {
-				log = data["log"] as NSString
-				log = log.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+				let decodedData = NSData.init(base64EncodedString: data["log"] as String, options: nil)
+				if let decodedString: NSString = NSString.init(data: decodedData!, encoding: NSUTF8StringEncoding) {
+					log = decodedString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+					self.clearBtn?.enabled = true
+				}
 				if log == "" {
 					log = Utils().lexicon("error_log_empty")
+					self.clearBtn?.enabled = false
 				}
 			}
 
-			self.textLabel?.text = log
+			self.textView.text = log
 		}
 	}
 
